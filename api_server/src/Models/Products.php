f77d2699f5863505805c1e17de92a5eb179ea108
中野雄貴 <totoro.windows@outlook.com>
2019-11-14 18:18:27 +0900

    jsonへの読み書き．(jsonへの書き込みができない)

--- api_server/src/Models/Products.php
+++ api_server/src/Models/Products.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 class Products
 {
-    public $db_date;
+    public $db_data;
+    private $db_file = '/db/db.json';
     public function __construct()
     {
         // $this->db_date = json_decode(file_get_contents("/Users/NakanoYuki/Projects/teamlab/teamlab_task_01/api_server/src/Models/db/db.json"), true);
@@ -12,10 +13,20 @@ class Products
         // return $this->db_date;
     }
 
-    public function getDate()
+    // データの取得
+    public function getData()
     {
         // return $this->db_date;
-        $this->db_date = json_decode(file_get_contents(__DIR__ . '/db/db.json'), true);
-        return $this->db_date;
+        $this->db_data = json_decode(file_get_contents(__DIR__ . $this->db_file), true);
+        return $this->db_data;
+    }
+
+    // データの追加
+    public function addData()
+    {
+        file_put_contents(__DIR__ . $this->db_file, json_encode($this->db_data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        // $db = fopen(__DIR__ . $this->db_file, 'w');
+        // fwrite($db, json_encode($this->db_data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        // fclose($db);
     }
 }
