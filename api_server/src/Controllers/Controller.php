f0b9753e50a27d65badfb6ba466eff3a6f2847f0
中野雄貴 <totoro.windows@outlook.com>
2019-11-14 18:20:47 +0900

    データベースの情報をグローバル変数に

--- api_server/src/Controllers/Controller.php
+++ api_server/src/Controllers/Controller.php
@@ -6,9 +6,15 @@ use Psr\Container\ContainerInterface;
 use Slim\Views\PhpRenderer;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Models\Products;
 
 abstract class Controller
 {
-    public function __construct(ContainerInterface $container)
-    { }
+    public $prodcut;
+    public $data;
+    public function __construct()
+    {
+        $this->prodcut = new Products();
+        $this->data = $this->prodcut->getData();
+    }
 }
